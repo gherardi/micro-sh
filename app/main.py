3a728c0454fcfbf09feb9915377b98dd9b680d94
Victor Gherardi <gherardivictor@gmail.com>
2026-07-11 13:21:31 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,6 +6,15 @@ def main():
             command = input("$ ")
         except EOFError:
             break
+
+        args = command.split()
+        if not args:
+            continue
+
+        if args[0] == "exit":
+            code = int(args[1]) if len(args) > 1 else 0
+            sys.exit(code)
+
         print(f"{command}: command not found")
 
 if __name__ == "__main__":
