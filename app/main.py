7a04759e2b968042f5004c7d8686b37466101221
Victor Gherardi <gherardivictor@gmail.com>
2026-07-11 13:06:22 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import sys
 
-
 def main():
-    sys.stdout.write("$ ")
-    pass
-
+    while True:
+        try:
+            command = input("$ ")
+        except EOFError:
+            break
+        print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
